9edc59e1c94083f8b8524dd2e866219c8555435c
aoi_yuki <aoi_yuki@orenomono.com>
2019-09-25 22:50:04 +0900

    merge

--- mytestsite/wsgi.py
+++ mytestsite/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
-from django.core.wsgi import get_wsgi_application
-
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..' )
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+
+
+#import os
+
+#from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mytestsite.settings')
+
+#application = get_wsgi_application()
